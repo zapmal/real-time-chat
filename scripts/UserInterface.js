17588e97631d3c9598ce49a2ffc0c487ac65bfd3
Manuel Zapata <mzxgd16@gmail.com>
2020-04-04 18:30:29 -0400

    Formatted date for messages

--- scripts/UserInterface.js
+++ scripts/UserInterface.js
@@ -3,12 +3,19 @@ class UserInterface {
         this.list = list;
     }
     render(data) {
-        // todo: format sent_at
+        // todo: format (properly) sent at.
+        const messageSentAt = data.sent_at.toDate();
+        const currentTime = new Date();
+        const difference = currentTime.getTime() - messageSentAt.getTime();
+
+        const howMuchTimeAgo = Math.round(((difference / 1000 / 60) / 60) / 24);
+
+
         const html = `
         <li class="list-group-item">
             <span class="username">${data.username}</span>
             <span class="message">${data.message} </span>
-            <div class="time">${data.sent_at.toDate()}</div>   
+            <div class="time">Sent ${howMuchTimeAgo} days ago</div>   
         </li>
         `;
 
