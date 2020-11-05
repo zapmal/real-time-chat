71c7b00783268234a6fcbea469329f2cc2fd1583
Zondazx <mzxgd16@gmail.com>
2020-11-05 16:37:00 -0400

    starting new version

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
-const chatList = document.querySelector(".chat-list"); 
+const chatList = document.querySelector(".chat-list");
 const messageInput = document.querySelector(".new-chat");
 const usernameInput = document.querySelector(".new-name");
 const updateUsername = document.querySelector(".update-message");
 // Using event delegation.
-const rooms = document.querySelector(".chat-rooms");
+const rooms = document.querySelector(".chat-rooms")
+
+const username = localStorage.username ? localStorage.username : "ANON";
+const userInterface = new UserInterface(chatList);
+const chat = new Chat("general", username);
+
+chat.getMessages(message => userInterface.render(message));
 
 messageInput.addEventListener("submit", e => {
     e.preventDefault();
@@ -22,20 +28,12 @@ usernameInput.addEventListener("submit", e => {
 
     updateUsername.innerText = `Your name was updated, new username: ${newUsername}`;
     setTimeout(() => updateUsername.innerText = "", 3000);
-}); 
+});
 
 rooms.addEventListener("click", e => {
     if (e.target.tagName === "BUTTON") {
         userInterface.clear();
         chat.updateRoom(e.target.getAttribute("id"));
         chat.getMessages(message => userInterface.render(message));
-    }  
+    }
 });
-
-
-const username = localStorage.username ? localStorage.username : "ANON";
-const userInterface = new UserInterface(chatList);
-const chat = new Chat("general", username);
-
-chat.getMessages(message => userInterface.render(message));
-
